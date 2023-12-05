098719f7d7ee888e780f3c793bc457ec6ea708f4
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-06 01:29:06 +0530

    Refactor ( Homepage ) : Code Cleaning

--- src/screens/Homepage.tsx
+++ src/screens/Homepage.tsx
@@ -1,7 +1,8 @@
-import { Icon, Input } from "@rneui/themed";
-import { View, Text, Image } from "react-native";
+import { Icon } from "@rneui/themed";
+import { View, Text } from "react-native";
 import { FlatList, GestureHandlerRootView } from "react-native-gesture-handler";
 import React, { useEffect, useState } from "react";
+import { RestaurantCard, SearchInput } from "../components";
 
 interface Restaurant {
   id: string;
@@ -60,36 +61,6 @@ const Homepage = () => {
     },
   ];
 
-  const renderRestaurantCard = ({ item }: { item: Restaurant }) => (
-    <View
-      className="bg-white rounded-lg mb-8"
-      style={{
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 1,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 1,
-        elevation: 2,
-      }}
-    >
-      <Image
-        source={{ uri: `https://picsum.photos/200/300?random=${item.id}` }}
-        className="rounded-lg w-full h-64 mb-2"
-      />
-      <View className="flex-row items-center p-4">
-        <Text className="text-2xl font-bold">{item.name}</Text>
-        <View className="flex-row items-center ml-auto py-2 px-2 rounded-lg bg-orange-400">
-          <Text className="text-md text-white">
-            {item.rating}
-          </Text>
-          <Icon name="star" size={20} color="white" />
-        </View>
-      </View>
-    </View>
-  );
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View className="flex-row items-center p-2 gap-2">
@@ -98,31 +69,12 @@ const Homepage = () => {
       </View>
 
       <View className="m-2 rounded-2xl">
-        <Input
-          placeholder="Search"
-          leftIcon={<Icon name="search" size={32} color="orange" />}
-          containerStyle={{
-            borderWidth: 2,
-            borderColor: "gray",
-            borderRadius: 16,
-            padding: 8,
-          }}
-          renderErrorMessage={false}
-          inputContainerStyle={{
-            borderWidth: 0,
-            borderBottomWidth: 0,
-            gap: 16,
-          }}
-          inputMode="search"
-          inputStyle={{
-            fontSize: 20,
-          }}
-        />
+        <SearchInput />
       </View>
 
       <FlatList
         data={restaurants}
-        renderItem={renderRestaurantCard}
+        renderItem={RestaurantCard}
         keyExtractor={(item) => item.id}
         contentContainerStyle={{
           padding: 16,
