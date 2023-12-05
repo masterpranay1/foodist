9401febbd29200d1b64ef19b855c7e48a1a3039a
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-06 01:18:02 +0530

    Setup : Inter Font Family

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { StyleSheet, Text, SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
@@ -6,9 +7,18 @@ import store from "./src/state/store";
 import Home from "./src/navigation/navigation";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <Provider store={store}>
-      <SafeAreaView className="flex-1 mt-12">
+      <SafeAreaView className="flex-1 mt-12" >
         <StatusBar style="auto" />
         <Home />
       </SafeAreaView>
