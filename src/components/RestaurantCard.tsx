90cde2c92ba04e620a4f52e828e66e962ad941b4
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-06 14:36:33 +0530

    Update : Restaurant Card Component

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,5 +1,6 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
 
 interface Restaurant {
   id: string;
@@ -7,34 +8,44 @@ interface Restaurant {
   rating: number;
 }
 
-const RestaurantCard = ({ item }: { item: Restaurant }) => (
-  <View
-    className="bg-white rounded-2xl mb-8"
-    style={{
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 1,
-      },
-      shadowOpacity: 0.2,
-      shadowRadius: 5,
-      elevation: 2,
-    }}
-  >
-    <Image
-      source={{ uri: `https://picsum.photos/200/300?random=${item.id}` }}
-      className="rounded-2xl w-full h-64 mb-2"
-    />
-    <View className="flex-row items-center p-4">
-      <Text className="text-2xl font-bold">{item.name}</Text>
-      <View className="flex-row items-center ml-auto py-2 px-2 rounded-lg bg-green-500">
-        <Text className="text-md text-white">
-          {item.rating}
-        </Text>
-        <Icon name="star" size={20} color="white" />
-      </View>
+const RestaurantCard = ({ item }: { item: Restaurant }) => {
+
+  const navigation = useNavigation();
+
+  return (
+    <View
+      className="bg-white rounded-2xl mb-8"
+      style={{
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        elevation: 2,
+      }}
+    >
+      <Pressable
+        onPress={() => {
+          // @ts-ignore
+          navigation.navigate("RestaurantPage", { id: item.id });
+        }}
+      >
+        <Image
+          source={{ uri: `https://picsum.photos/200/300?random=${item.id}` }}
+          className="rounded-2xl w-full h-64 mb-2"
+        />
+        <View className="flex-row items-center p-4">
+          <Text className="text-2xl font-bold">{item.name}</Text>
+          <View className="flex-row items-center ml-auto py-2 px-2 rounded-lg bg-green-500">
+            <Text className="text-md text-white">{item.rating}</Text>
+            <Icon name="star" size={20} color="white" />
+          </View>
+        </View>
+      </Pressable>
     </View>
-  </View>
-);
+  );
+};
 
 export default RestaurantCard;
