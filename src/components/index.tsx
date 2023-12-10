b589a669508a54668d037019c0ff361f3bcb41fb
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-11 01:39:30 +0530

    fix : remove deleted component imports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
-import SearchInput from "./SearchInput";
 import DishCard from "./DishCard";
 import Text from "./Text";
 import { Input, InputWithIcon } from "./Input";
 
-export { RestaurantCard, SearchInput, DishCard, Text, Input, InputWithIcon };
+export { RestaurantCard, DishCard, Text, Input, InputWithIcon };
