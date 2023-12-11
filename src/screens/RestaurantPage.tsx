0ae725c15d4521ef64a16093aa1dc64d256f44ec
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-12 02:34:56 +0530

    Refactor : Restuarant Info To Res. Page

--- src/screens/RestaurantPage.tsx
+++ src/screens/RestaurantPage.tsx
@@ -1,8 +1,8 @@
 import { Icon } from "@rneui/themed";
-import { View, Text, Pressable } from "react-native";
-import { FlatList, GestureHandlerRootView, RefreshControl } from "react-native-gesture-handler";
+import { View, Pressable, ActivityIndicator } from "react-native";
+import { FlatList, RefreshControl } from "react-native-gesture-handler";
 import React, { useEffect, useState } from "react";
-import { RestaurantCard, DishCard, InputWithIcon } from "../components";
+import { DishCard, InputWithIcon, Text } from "../components";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { POCKETBASE } from "../constants";
 
@@ -13,12 +13,43 @@ type DishCardProps = {
   type: "veg" | "non-veg";
 };
 
+interface Restaurant {
+  id: string;
+  name: string;
+  rating: string;
+  categories: [string];
+}
+
 const RestaurantPage = () => {
   const [dishes, setDishes] = useState<DishCardProps[]>([]);
+  const [restaurant, setRestaurant] = useState<Restaurant>();
   const [refreshing, setRefreshing] = useState(false);
 
   const route = useRoute();
   const { id } = route.params as { id: string };
+
+  // fetch a single restaurant
+
+  const fetchRestaurant = async () => {
+    try {
+      setRefreshing(true);
+      const res = await fetch(
+        `${POCKETBASE}/api/collections/restaurants/records?filter=(id='${id}')`
+      );
+      const data = await res.json();
+      const restaurant: Restaurant = {
+        id: data.items[0].id,
+        name: data.items[0].name,
+        rating: data.items[0].rating,
+        categories: data.items[0].categories.split(","),
+      };
+      setRestaurant(restaurant);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setRefreshing(false);
+    }
+  };
 
   const fetchDishes = async () => {
     try {
@@ -43,53 +74,108 @@ const RestaurantPage = () => {
 
   useEffect(() => {
     fetchDishes();
+    fetchRestaurant();
   }, []);
+
+  useEffect(() => {
+    if (restaurant && restaurant.rating) {
+      if (!(restaurant.rating.split(".").length > 1)) {
+        setRestaurant({ ...restaurant, rating: restaurant.rating + ".0" });
+      }
+    }
+  }, [restaurant]);
 
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1 }}>
-      <View className="flex-row items-center pb-2 px-2 border-b border-slate-200">
-        <Pressable
-          onPress={() => {
-            navigation.goBack();
-          }}
-        >
-          <Icon
-            name="left"
-            size={32}
-            color="rgba(200,0,0, 0.8)"
-            type="ant-design"
-          />
-        </Pressable>
-        <View className="rounded-2xl w-5/6 mx-auto">
-          <InputWithIcon
-            iconName="search"
-            placeholder="Search for dishes"
-          />
-        </View>
-      </View>
-
-      {dishes.length > 0 ? (
-        <FlatList
-          data={dishes}
-          renderItem={({ item }) => (
-            <DishCard
-              dishTitle={item.dishTitle}
-              price={item.price}
-              description={item.description}
-              type={item.type}
-            />
-          )}
-          keyExtractor={(item) => item.dishTitle}
-          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={fetchDishes} />}
-        />
-      ) : (
+    <>
+      {(!restaurant || !dishes) && (
         <View className="flex-1 items-center justify-center">
-          <Text className="text-3xl font-bold">No Dishes</Text>
+          <ActivityIndicator size="large" color="rgba(200,0,0, 0.8)" />
         </View>
       )}
-    </View>
+      <View style={{ flex: 1 }}>
+        <View className="flex-row items-center pb-2 px-2 border-b border-slate-200">
+          <Pressable
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Icon
+              name="left"
+              size={32}
+              color="rgba(200,0,0, 0.8)"
+              type="ant-design"
+            />
+          </Pressable>
+          <View className="rounded-2xl w-5/6 mx-auto">
+            <InputWithIcon iconName="search" placeholder="Search for dishes" />
+          </View>
+        </View>
+
+        <View className="flex-col gap-1 items-center justify-center pb-8 my-4 border-b-8 border-slate-200">
+          <Text className="text-2xl" thickness="bold">
+            {restaurant?.name}
+          </Text>
+          <View className="flex-row items-center">
+            {restaurant?.categories.map((category, index) => (
+              <React.Fragment key={index}>
+                <Text
+                  key={index}
+                  className="text-xs text-slate-400"
+                  thickness="medium"
+                >
+                  {category}
+                </Text>
+
+                {restaurant?.categories.length > 1 &&
+                  index < restaurant?.categories.length - 1 && (
+                    <Icon
+                      name="dot-single"
+                      size={24}
+                      color="rgba(0,0,0,0.2)"
+                      type="entypo"
+                    />
+                  )}
+              </React.Fragment>
+            ))}
+          </View>
+          <View className="flex-row items-center">
+            <View className="flex-row items-center mt-2 p-1 rounded-lg bg-[#24963f]">
+              <Text className="text-xs text-white" thickness="bold">
+                {restaurant?.rating}
+              </Text>
+              <Icon name="star" size={12} color="white" />
+            </View>
+            <Text className="ml-4 border-b border-dashed border-slate-200 text-slate-400">
+              4.5k ratings
+            </Text>
+          </View>
+        </View>
+
+        {dishes.length > 0 ? (
+          <FlatList
+            data={dishes}
+            renderItem={({ item }) => (
+              <DishCard
+                dishTitle={item.dishTitle}
+                price={item.price}
+                description={item.description}
+                type={item.type}
+              />
+            )}
+            keyExtractor={(item) => item.dishTitle}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={fetchDishes} />
+            }
+          />
+        ) : (
+          <View className="flex-1 items-center justify-center">
+            <Text className="text-3xl font-bold">No Dishes</Text>
+          </View>
+        )}
+      </View>
+    </>
   );
 };
 
