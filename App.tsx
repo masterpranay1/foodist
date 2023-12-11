8bd9988238d1e268c176e207c0b6aeee9e63159a
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-12 00:15:25 +0530

    feat : Root Gesture controller component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
-import { useFonts,Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import {
+  useFonts,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
 import store from "./src/state/store";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Home from "./src/navigation/navigation";
 
@@ -12,7 +20,7 @@ export default function App() {
     Inter_400Regular,
     Inter_500Medium,
     Inter_700Bold,
-    Inter_800ExtraBold
+    Inter_800ExtraBold,
   });
 
   if (!fontsLoaded) {
@@ -21,9 +29,11 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <SafeAreaView className="flex-1 mt-12" >
-        <StatusBar style="auto" />
-        <Home />
+      <SafeAreaView className="flex-1 mt-12">
+          <StatusBar style="auto" />
+        <GestureHandlerRootView className="flex-1">
+          <Home />
+        </GestureHandlerRootView>
       </SafeAreaView>
     </Provider>
   );
