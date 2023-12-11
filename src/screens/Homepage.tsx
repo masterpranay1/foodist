81dbcc02b4a2fe1962f61ee96fe78a8cec673d56
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-12 00:19:28 +0530

    Refactor( style ) : Hompage Header

--- src/screens/Homepage.tsx
+++ src/screens/Homepage.tsx
@@ -1,18 +1,19 @@
 import { Icon } from "@rneui/themed";
-import { View, Text } from "react-native";
+import { View, TouchableWithoutFeedback, Keyboard } from "react-native";
 import {
   FlatList,
   GestureHandlerRootView,
   RefreshControl,
 } from "react-native-gesture-handler";
 import React, { useEffect, useState } from "react";
-import { RestaurantCard, InputWithIcon } from "../components";
+import { RestaurantCard, InputWithIcon, Text } from "../components";
 import { POCKETBASE } from "../constants";
 
 interface Restaurant {
   id: string;
   name: string;
-  rating: number;
+  rating: string;
+  categories: [string];
 }
 
 const Homepage = () => {
@@ -28,7 +29,6 @@ const Homepage = () => {
 
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
   const [refreshing, setRefreshing] = useState(false);
-  
   const fetchRestaurants = async () => {
     try {
       setRefreshing(true);
@@ -40,6 +40,7 @@ const Homepage = () => {
         id: item.id,
         name: item.name,
         rating: item.rating,
+        categories: item.categories.split(","),
       }));
       setRestaurants(restaurants);
     } catch (err) {
@@ -54,34 +55,52 @@ const Homepage = () => {
   }, []);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View className="py-2 px-4 shadow-xl shadow-black bg-white">
-        <View className="flex-row items-center">
-          <Icon name="location-pin" size={32} color="rgba(200, 0, 0, 0.8)" />
-          <Text className="text-md ml-2">{address}</Text>
-        </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View className="flex-1">
+        <View className="pb-2 px-4 bg-white">
+          <View className="flex-row items-center gap-2">
+            <Icon name="location-pin" size={32} color="rgba(200, 0, 0, 0.8)" />
 
-        <View className=" rounded-2xl">
-          <InputWithIcon
-            iconName="search"
-            placeholder="Search for restaurants"
-          />
+            <View>
+              <View className="flex-row items-center">
+                <Text className="text-base" thickness="extra-bold">
+                  Home
+                </Text>
+                <Icon name="chevron-small-down" size={24} type="entypo" />
+              </View>
+              <Text className="text-slate-400 text-xs" thickness="medium">
+                {address}
+              </Text>
+            </View>
+          </View>
+
+          <View className="rounded-2xl mt-1">
+            <InputWithIcon
+              iconName="search"
+              placeholder="Search for restaurants"
+            />
+          </View>
         </View>
+        {restaurants.length > 0 ? (
+          <FlatList
+            data={restaurants}
+            renderItem={({ item }) => <RestaurantCard item={item} />}
+            keyExtractor={(item) => item.id}
+            contentContainerStyle={{ padding: 16 }}
+            refreshControl={
+              <RefreshControl
+                onRefresh={fetchRestaurants}
+                refreshing={refreshing}
+              />
+            }
+          />
+        ) : (
+          <Text className="text-center text-2xl font-bold mt-4">
+            No restaurants found
+          </Text>
+        )}
       </View>
-      {restaurants.length > 0 ? (
-        <FlatList
-          data={restaurants}
-          renderItem={({ item }) => <RestaurantCard item={item} />}
-          keyExtractor={(item) => item.id}
-          contentContainerStyle={{ padding: 16 }}
-          refreshControl={<RefreshControl onRefresh={fetchRestaurants} refreshing={refreshing} />}
-        />
-      ) : (
-        <Text className="text-center text-2xl font-bold mt-4">
-          No restaurants found
-        </Text>
-      )}
-    </GestureHandlerRootView>
+    </TouchableWithoutFeedback>
   );
 };
 
