0e92bf3abb1dc7a01a27f3ea756db7474ad9e651
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-14 05:23:32 +0530

    Update : Splash Screen

--- App.tsx
+++ App.tsx
@@ -11,8 +11,11 @@ import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
 import store from "./src/state/store";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from "expo-splash-screen";
 
 import Home from "./src/navigation/navigation";
+
+SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,8 +32,13 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <SafeAreaView className="flex-1 mt-12">
-          <StatusBar style="auto" />
+      <SafeAreaView
+        className="flex-1 mt-12"
+        onLayout={() => {
+          SplashScreen.hideAsync().catch(() => {});
+        }}
+      >
+        <StatusBar style="auto" />
         <GestureHandlerRootView className="flex-1">
           <Home />
         </GestureHandlerRootView>
