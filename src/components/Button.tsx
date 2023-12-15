cc2bd57cca1ef66cbbeae366425038036b8df661
masterpranay1 <rajpranaychottu@gmail.com>
2023-12-16 01:05:13 +0530

    refactor: button disable prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,34 +1,55 @@
-
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import React from "react";
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
 
 interface ButtonProps {
   title: string;
   onPress: () => void;
   loading?: boolean;
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ title, onPress, loading=false }) => {
+const Button: React.FC<ButtonProps> = ({
+  title,
+  onPress,
+  loading = false,
+  disabled = false,
+}) => {
   return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
+    <TouchableOpacity
+      style={disabled ? styles.buttonDisabled : styles.button}
+      onPress={onPress}
+    >
       {!loading && <Text style={styles.buttonText}>{title}</Text>}
-      {loading && <ActivityIndicator color="white" size={"large"}/>}
+      {loading && <ActivityIndicator color="white" size={"large"} />}
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: '#d33333',
+    backgroundColor: "#d33333",
     padding: 10,
     borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonDisabled: {
+    backgroundColor: "#d33333",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    opacity: 0.5,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-    fontFamily: 'Inter_400Regular',
+    fontFamily: "Inter_400Regular",
   },
 });
 
